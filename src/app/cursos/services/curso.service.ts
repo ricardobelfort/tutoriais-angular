39b1368035ee38357ccbd7ee14708529cd22902e
Ricardo Belfort <rbelfort2004@gmail.com>
2023-04-26 21:24:59 -0400

    feat: add env files

--- src/app/cursos/services/curso.service.ts
+++ src/app/cursos/services/curso.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Curso } from '../models/curso';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CursoService {
-  private readonly API = 'http://localhost:3000';
+  private readonly API = environment.API;
 
   constructor(private http: HttpClient) {}
 
   list() {
-    return this.http.get<Curso[]>(`${this.API}/cursos`);
+    return this.http.get<Curso[]>(`${this.API}cursos`);
   }
 }
